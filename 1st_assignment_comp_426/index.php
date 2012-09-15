49ddb6e1c3b34a16d1157bc97f0b2abd2f48f58d
Matt Corallo <git@bluematt.me>
2012-09-15 18:25:11 -0400

    Unix line endings (so git diff doesnt have wierd crap in it)

--- 1st_assignment_comp_426/index.php
+++ 1st_assignment_comp_426/index.php
@@ -3,7 +3,7 @@
     <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
     <title>UNC Carpooling</title>
 
-    <link href="style.css" rel="stylesheet" type="text/css">
+    <link href="_style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
