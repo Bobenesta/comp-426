306d4e63bba705a2b41f8d9efbf4bf695e33ba91
Matt Corallo <git@bluematt.me>
2012-09-15 18:26:59 -0400

    Cleanup index.php (no changes)

--- 1st_assignment_comp_426/index.php
+++ 1st_assignment_comp_426/index.php
@@ -1,49 +1,37 @@
-﻿<!DOCTYPE html>
+<html>
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-    <title>UNC Carpooling</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>UNC Carpooling</title>
 
-    <link href="_style.css" rel="stylesheet" type="text/css">
+	<link href="style.css" rel="stylesheet" type="text/css">
 </head>
 
 <body>
 
 <div class="content">
-    <div id="intro">
-         <H2>UNC Carpooling</H2>
-         <br>
-         <span> intro intro </span>
-    </div>
+	<div id="intro">
+		<H2>UNC Carpooling</H2>
+		<br>
+		<span> intro intro </span>
+	</div>
 
-    <div id="login-box">
-        <form name="user_login" method="" action="">
-        <H2>UNC Carpooling</H2>
-        <br>Best Carpooling Service at UNC.
-        <br>
-        <br>
-        <div id="login-box-name" style="margin-top:20px;">Username:</div><div id="login-box-field" style="margin-top:20px;"><input name="user_name" id="username" class="form-login" title="Username" value="" size="30" maxlength="2048" /></div>
-        <div id="login-box-name">Password:</div><div id="login-box-field"><input name="user_password" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" /></div>
-        <br>
-        <span class="login-box-options"><input type="checkbox" name="1" value="1"> Remember Password <a href="user_reg.php" style="margin-left:30px;">Registration</a></span>
-        <br>
-        <br>
-        </form>
-        <a href="#" onclick="document.forms[0].submit();"><img src="images/login-btn.png" width="103" height="42" style="margin-left:90px;"></a>
+	<div id="login-box">
+		<form name="user_login" method="" action="">
+			<H2>UNC Carpooling</H2>
+			<br>Best Carpooling Service at UNC.
+			<br>
+			<br>
+			<div id="login-box-name" style="margin-top:20px;">Username:</div><div id="login-box-field" style="margin-top:20px;"><input name="user_name" id="username" class="form-login" title="Username" value="" size="30" maxlength="2048" /></div>
+			<div id="login-box-name">Password:</div><div id="login-box-field"><input name="user_password" type="password" class="form-login" title="Password" value="" size="30" maxlength="2048" /></div>
+			<br>
+			<span class="login-box-options"><input type="checkbox" name="1" value="1"> Remember Password <a href="user_reg.php" style="margin-left:30px;">Registration</a></span>
+			<br>
+			<br>
+		</form>
+		<a href="#" onclick="document.forms[0].submit();"><img src="images/login-btn.png" width="103" height="42" style="margin-left:90px;"></a>
     </div>
 
 </div>
-
-
-
-
-
-
-
-
-
-
-
-
 
 </body>
 </html>
