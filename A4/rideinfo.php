00771742222d7a72d288f32d86115778ea09a911
Matt Corallo <git@bluematt.me>
2012-10-21 14:45:00 -0400

    new rideinfo page

---
+++ A4/rideinfo.php
@@ -0,0 +1,26 @@
+<!DOCTYPE HTML>
+<html lang=en>
+<head>
+	<meta charset=utf-8>
+	<title>Find A Ride</title>
+
+	<link rel="stylesheet" href="style.css" />
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
+	<script type="text/javascript" src="userinfo.js"></script>
+	<script type="text/javascript">
+
+	</script>
+</head>
+
+<body>
+	<?php require("header.php"); ?>
+
+	<div id="content">
+	<div class="content-box">
+		
+	</div>
+	</div>
+
+	<?php require("footer.php"); ?>
+</body>
+</html
