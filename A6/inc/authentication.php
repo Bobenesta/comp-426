7b9fa5ab615f33f1751194ef5e5f94acdbbabcf0
Matt Corallo <git@bluematt.me>
2012-11-25 20:57:46 -0500

    id not userId

--- A6/inc/authentication.php
+++ A6/inc/authentication.php
@@ -11,13 +11,17 @@ if (!is_null($_SESSION['userIdAuthenticated']))
 
 function authenticate($userName, $passwordHash) {
 	$mysqli = getDBConnection();
-	$result = $mysqli->query("SELECT userId, passwordHash FROM users WHERE userName = " .
+	$result = $mysqli->query("SELECT id, passwordHash FROM users WHERE userName = " .
 					$mysqli->real_escape_string($userName));
 	if ($result) {
 		if ($result->num_rows == 0)
 			return 0;
 
 		$row = $result->fetch_assoc();
+
+		// Though not explicity stated, $passwordHash should be
+		// hex(sha256(password + "gEp3XuY9r7ajWxSIG7mW04PHlL9JxqXhhVs"))
+		// eg password1 becomes b9e108874aa0b938cb42d07fca687cbad465175713d62447e1371f2854edf7f2
 
 		if (hash("sha256", $passwordHash . "35dlLfZQpUyLJf9KBARov85GvtlHkhwTin8", true) == $row['passwordHash']) {
 			$userIdLoggedIn = intval($row['userId']);
