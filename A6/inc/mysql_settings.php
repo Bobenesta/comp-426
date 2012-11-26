531a40e76d98f19e01273a86d3050014a1ad726e
Matt Corallo <git@bluematt.me>
2012-11-25 20:09:40 -0500

    comp42624db

--- A6/inc/mysql_settings.php
+++ A6/inc/mysql_settings.php
@@ -1,7 +1,7 @@
 <?php
 
 function getDBConnection() {
-	$mysqli = new mysqli("classroom.cs.unc.edu", "corallo", "z3tnx5dJd8pJ9KfH", //TODO DBNAME);
+	$mysqli = new mysqli("classroom.cs.unc.edu", "corallo", "z3tnx5dJd8pJ9KfH", "comp42624db");
 	if ($mysqli)
 		return $mysqli;
 
