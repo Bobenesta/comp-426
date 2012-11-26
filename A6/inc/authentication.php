eee3fd9a290f7ff7c52cf599bcc02978d6c82a9a
Matt Corallo <git@bluematt.me>
2012-11-26 15:40:54 -0500

    real pass

--- A6/inc/authentication.php
+++ A6/inc/authentication.php
@@ -21,7 +21,7 @@ function authenticate($userName, $passwordHash) {
 
 		// Though not explicity stated, $passwordHash should be
 		// hex(sha256(sha256(password + "gEp3XuY9r7ajWxSIG7mW04PHlL9JxqXhhVs") + $_SESSION['loginSalt'])
-		// eg password1 becomes sha256(0xb9e108874aa0b938cb42d07fca687cbad465175713d62447e1371f2854edf7f2 + $_SESSION['loginSalt'])
+		// eg password1 becomes sha256(0x8e4365b5d8a26e4f22eab048daf77e0c30134745ce50c700f866fb537143c1fd + $_SESSION['loginSalt'])
 
 		if (hash("sha256", bin2hex($row['passwordHash']) . $_SESSION['loginSalt']) == $passwordHash) {
 			$userIdLoggedIn = intval($row['id']);
