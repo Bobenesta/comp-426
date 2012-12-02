799f9d06595d281db9488bab4f70ea529fa758dd
Matt Corallo <git@bluematt.me>
2012-12-02 16:05:28 -0500

    Missing jquery

--- A7/homepage.php
+++ A7/homepage.php
@@ -5,6 +5,7 @@
 	<title>First assignment comp 426 - carpooling web application</title>
 
 	<link rel="stylesheet" href="style.css"/>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.8.1/jquery.min.js"></script>
 </head>
 
 <body>
