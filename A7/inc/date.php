5f1bfead844953ade4ccbe834e80b46f853dc3be
Matt Corallo <git@bluematt.me>
2012-12-02 23:04:01 -0500

    Apparently its in the same format...

--- A7/inc/date.php
+++ A7/inc/date.php
@@ -1,8 +1,6 @@
 <?php
 function validateConvertDateFromWireToMySQL($date) {
-	$tmp = explode("/",$date);
-	if (count($tmp) != 3)
-                $tmp = explode("-",$date);
+	$tmp = explode("-",$date);
 
 	if (count($tmp) != 3)
 		return null;
@@ -10,7 +8,7 @@ function validateConvertDateFromWireToMySQL($date) {
 	if(!is_numeric($tmp[0]) || !is_numeric($tmp[1]) || !is_numeric($tmp[2]))
 		return null;
 
-	return $tmp[2] . "-" . $tmp[0] . "-" . $tmp[1];
+	return $tmp[0] . "-" . $tmp[1] . "-" . $tmp[2];
 }
 
 function validateConvertDateFromMySQLToWire($date) {
@@ -21,6 +19,6 @@ function validateConvertDateFromMySQLToWire($date) {
 	if(!is_numeric($tmp[0]) || !is_numeric($tmp[1]) || !is_numeric($tmp[2]))
 		return null;
 
-	return $tmp[1] . "/" . $tmp[2] . "/" . $tmp[0];
+	return $tmp[0] . "-" . $tmp[1] . "-" . $tmp[2];
 }
 ?>
