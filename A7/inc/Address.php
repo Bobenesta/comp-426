0090ea726b65c2fa841a6cd654b89b90567e3f8f
Matt Corallo <git@bluematt.me>
2012-12-05 18:35:24 -0500

    Thar we go

--- A7/inc/Address.php
+++ A7/inc/Address.php
@@ -66,7 +66,7 @@ class Address {
 				if ($result->num_rows == 0) {
 print("0\n");
 print("USA, " . $state . ", " . $city . ", " . $addressLine);
-					$location = googleHelper.getCoordinates("USA, " . $state . ", " . $city . ", " . $addressLine);
+					$location = googleHelper->getCoordinates("USA, " . $state . ", " . $city . ", " . $addressLine);
 print("99\n");
 					if ($location == null)
 						return null;
