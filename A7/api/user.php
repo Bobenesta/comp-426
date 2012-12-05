83aa76e7929a2ac7d97c4dca45b1487bdbec84c1
Jack Wang <zhew@live.unc.edu>
2012-12-05 16:43:10 -0500

    temp1

--- A7/api/user.php
+++ A7/api/user.php
@@ -2,5 +2,21 @@
 require_once("../inc/require_authentication.php");
 
 $id= $_GET['profileId'];
-echo($id);
+echo(getById($id)+"'s profile");
+
+public static function getById($id) {
+		$mysqli = getDBConnection();
+		$result = $mysqli->query("SELECT * FROM users WHERE id = '" . $id . "'");
+		if ($result) {
+			if ($result->num_rows == 0)
+				return null;
+
+			$row = $result->fetch_assoc();
+
+			return $row['userName']);
+		}
+		return null;
+	}
+
+
 ?>
