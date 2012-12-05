e0018f0568727c2f47a95efc1f7f264d92495189
Matt Corallo <git@bluematt.me>
2012-12-05 16:31:06 -0500

    window.location not jquery

--- A7/authentication.js
+++ A7/authentication.js
@@ -24,7 +24,7 @@ function login(user, password) {
 									passwordHash: passwordHash
 								},
 								success: function(data, textStatus, jqXHR) {
-										$(location).attr('href', "landing.php");
+										window.location.href = landing.php;
 									},
 								error: loginFailureHandler,
 								cache: false
@@ -45,7 +45,7 @@ function logout() {
 			{
 				type: 'GET',
 				success: function(data, textStatus, jqXHR) {
-						$(location).attr('href', "homepage.php");
+						window.location.href = "homepage.php";
 					},
 				error: function(jqXHR, textStatus, errorThrown) {
 						alert("Error connecting to backend, please try again later");
