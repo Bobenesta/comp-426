abde8b271bebb00f4ccf0a6bd0d71582f9112ae9
Matt Corallo <git@bluematt.me>
2012-12-05 20:39:31 -0500

    Update requestinfo

--- A7/request.js
+++ A7/request.js
@@ -27,14 +27,14 @@ Request.display= function(requestId){
 						$("#rideinfo-from").html(Address.prototype.getShortName.call(data.fromAddress));
 						$("#rideinfo-car-capacity").html(data.carCapacity);
 						$("#rideinfo-car-desc").html(data.carDesc);
+						$("#rideinfo-name").html(data.user.name);
+						$("#rideinfo-rating").html(data.user.rating);
 					},
 				error: function(data, textStatus, jqXHR) {
 						alert("Error getting ride info, please try again later.");
 					},
 				cache: false
 			});
-
-	$("#rideinfo-name").html(Request.getRequestById(requestId).userId);
 
 	var iframe="<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr="+Request.getRequestById(requestId).from+"&amp;daddr="+Request.getRequestById(requestId).to+"&amp;hl=en&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639&amp;output=embed'></iframe><br /><small><a href='https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr="+Request.getRequestById(requestId).from+"&amp;daddr="+Request.getRequestById(requestId).to+"&amp;hl=en&amp;sll=35.95363,-78.990775&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639' style='color:#0000FF;text-align:left'>View Larger Map</a></small>";
 	$("#map").html(iframe);
