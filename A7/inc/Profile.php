71b92144a741cceeca0eea654cf54250dd5b3788
Jack Wang <zhew@live.unc.edu>
2012-12-05 20:13:00 -0500

    update

--- A7/inc/Profile.php
+++ A7/inc/Profile.php
@@ -5,13 +5,11 @@ require_once("../inc/Ride.php");
 
 class Profile{
 	private $name;
-	private $requests;
-	private $rides;
+	private $rating;
 	
-	private function __construct($name, $requests, $rides) {
+	private function __construct($name, $rating) {
 		$this->name = $name;
-		$this->requests = $requests;
-		$this->rides = $rides;
+		$this->rating = $rating;
 	}
 	
 public function getNameById($id){
@@ -30,64 +28,41 @@ public function getNameById($id){
 		return null;
 }
 
-public function getRequestByUserId($userid){
-	$mysqli = getDBConnection();
-	$result = $mysqli->query("SELECT * FROM requests WHERE userId = '" . $id . "'");
-	
-	if ($row = $result->fetch_assoc()) {
-		return Request::getById($row['userid']);
-    }
+public function getRating($id){
+	$name= "";
+		$mysqli = getDBConnection();
+		$result = $mysqli->query("SELECT * FROM rating WHERE userId = '" . $id . "'");
+		if ($result) {
+			if ($result->num_rows == 0)
+				return null;
 
+			$row = $result->fetch_assoc();
+
+			$name= $row['rating'];
+			return $name;
+		}
+		return null;
 }
 
-public function getOfferByUserId($userid){
-	$mysqli = getDBConnection();
-	$result = $mysqli->query("SELECT * FROM rides WHERE userId = '" . $id . "'");
-	
-	if ($row = $result->fetch_assoc()) {
-		return Ride::getById($row['userId']);
-    }
-}
 
 public static function create($id){
 	$name= getNameById($id);
 	if(is_null($name)){
 		return null;
 	}
-	$request= getRequestByUserId($id);
-	if(is_null($request)){
+	$rating= getRatingByUserId($id);
+	if(is_null($rating)){
 		return null;
 	}
-	$ride= getOfferByUserId($id);
-	if(is_null($ride)){
-		return null;
-	}
-	return new Profile($name, $request, $ride);
+	return new Profile($name, $rating);
 }
 
 public static function getJSON(){
 	$ret= array();
 		$ret['name'] = $this->name;
-		$ret['requests'] = $this->requests->getJSON();
-		$ret['rides'] = $this->rides->getJSON();
+		$ret['rating'] = $this->rating;
 		return $ret;
 }
 }
-
-// 
- // echo("<br><br><tr><td> From </td><td> Rating </td><td> Message </td></tr>");
-// if ($result = $mysqli->query("SELECT * FROM ratings WHERE userTo = '" . $id . "'")) {
-// echo $result->num_rows;
-    // /* fetch object array */
-    // while ($row = $result->fetch_assoc()) {
-    	// $from= $mysqli->query("SELECT userName FROM users WHERE id = '" . $row['userFrom'] . "'");
-		// $rating= $row['rating'];
-		// $message= $row['message'];
-        // echo("<tr><td>".$from."</td><td>".$rating."</td><td>".$message."</td></tr><br>");
-    // }
-// 
-    // /* free result set */
-    // $result->close();
-// }
 
 ?>
