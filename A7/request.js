a244288b1c5e80914754f4ce1411a3574759eb46
Matt Corallo <git@bluematt.me>
2012-12-05 20:40:36 -0500

    Fuck the map

--- A7/request.js
+++ A7/request.js
@@ -36,6 +36,9 @@ Request.display= function(requestId){
 				cache: false
 			});
 
-	var iframe="<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr="+Request.getRequestById(requestId).from+"&amp;daddr="+Request.getRequestById(requestId).to+"&amp;hl=en&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639&amp;output=embed'></iframe><br /><small><a href='https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr="+Request.getRequestById(requestId).from+"&amp;daddr="+Request.getRequestById(requestId).to+"&amp;hl=en&amp;sll=35.95363,-78.990775&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639' style='color:#0000FF;text-align:left'>View Larger Map</a></small>";
-	$("#map").html(iframe);
+	/*var iframe="<iframe width='425' height='350' frameborder='0' scrolling='no' marginheight='0' marginwidth='0' src='https://maps.google.com/maps?f=d&amp;source=s_d&amp;saddr="+
+		Request.getRequestById(requestId).from+
+		"&amp;daddr="+Request.getRequestById(requestId).to+
+		"&amp;hl=en&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639&amp;output=embed'></iframe><br /><small><a href='https://maps.google.com/maps?f=d&amp;source=embed&amp;saddr="+Request.getRequestById(requestId).from+"&amp;daddr="+Request.getRequestById(requestId).to+"&amp;hl=en&amp;sll=35.95363,-78.990775&amp;sspn=0.157579,0.338173&amp;t=h&amp;mra=ls&amp;ie=UTF8&amp;ll=35.95363,-78.990775&amp;spn=0.11038,0.10639' style='color:#0000FF;text-align:left'>View Larger Map</a></small>";
+	$("#map").html(iframe);*/
 }
